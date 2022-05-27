60f43b1581de09f9ad64e1ef81951814b8429a60
sea <artur.semenenko@nag.ch>
2022-05-27 15:49:39 +0200

    Finish http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {DishesPageComponent} from './dishes-page/dishes-page.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    DishesPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
